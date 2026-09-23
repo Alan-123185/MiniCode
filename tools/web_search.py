fcfc5acf45f46a6b2841d3b5c38ba53fb6b6cbcf
Yi-Chenyu <3388364100@qq.com>
2026-09-23 17:08:45 +0800

    9.23 搜索增强

--- tools/web_search.py
+++ tools/web_search.py
@@ -1,9 +1,11 @@
+import httpx
 import requests
 import json
 
 from langchain_core.tools import tool
 from config.data import settings
 from core.toolResult import toolResult
+from utils.MessageTool import compress_error
 
 
 @tool
@@ -62,7 +64,7 @@ def baidu_search(question : str) -> toolResult:
         ref["url"]=reference["url"]
         ref["title"]=reference["title"]
         ref["date"]=reference["date"]
-        ref["content"]=reference["snippet"]
+        ref["content"]=_compress_search_result(reference["snippet"])
         if ref["content"] not in result:
             result.append(ref)
     return toolResult(
@@ -73,8 +75,64 @@ def baidu_search(question : str) -> toolResult:
     )
 
 
-if __name__ == '__main__':
-    res = baidu_search.invoke({"question":"长沙今天天气如何"})
-    for i in res.content:
-       print(i.content)
+#@tool
+def Jina_search(url:str,time_out:int=settings.web_search_timeout) -> toolResult:
+    """
+    使用Jina AI进行网页内容搜索
 
+    :param url: 要搜索的网页URL
+    :time_out: 请求超时时间，可以自行调节，单位为秒，默认为30秒
+    :return:  返回一个工具调用结果类
+    """
+    head="https://r.jina.ai/"
+    for time in range(3):
+        try:
+            result = httpx.get(head + url,timeout=time_out,follow_redirects=True)
+            result.raise_for_status()
+            return toolResult(
+                success=True,
+                message=f"{url}的搜索结果",
+                content=f"{_compress_search_result(result.text)}",
+                tool_name="Jina_search"
+            )
+        except Exception as e:
+            time_out+=7
+            if time==2:
+                return toolResult(
+                    success=False,
+                    content=f"请求失败: {compress_error(str(e))}",
+                    tool_name="Jina_search"
+                )
+            continue
+    return toolResult(
+        success=False,
+        content=f"请求失败: unknown error",
+        tool_name="Jina_search"
+    )
+
+
+
+
+def _compress_search_result(content:str) -> str:
+    """
+    压缩搜索结果，避免过长的内容影响后续处理
+    """
+    if len(content)<=settings.web_search_words_limit:
+        return content
+    pos=content.rfind("\n",0,settings.web_search_words_limit)
+    if pos!=-1:
+        content = content[:pos] + "...(内容过长，已截断)"
+    else:
+        content = content[:settings.web_search_words_limit] + "...(内容过长，已截断)"
+    return content
+
+
+
+
+
+if __name__ == '__main__':
+    # res = baidu_search.invoke({"question":"长沙今天天气如何"})
+    # for i in res.content:
+    #    print(i.content)
+    res = Jina_search("https://github.com/psf/requests/issues/4392",30)
+    print(res)
